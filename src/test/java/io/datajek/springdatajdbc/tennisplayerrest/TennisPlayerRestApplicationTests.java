3fcf6ac49499ac002cbad2eac50aee3f5b848d4e
Geoffrey Brophy <geoffrey.brophy@proton.me>
2025-03-27 17:43:48 -0400

    Initial commit

---
+++ src/test/java/io/datajek/springdatajdbc/tennisplayerrest/TennisPlayerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.springdatajdbc.tennisplayerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayerRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
